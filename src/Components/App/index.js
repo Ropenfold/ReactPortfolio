4d5c2c8ee09dc139c8b2be1eae4a58e37fbe11bc
Ropenfold <stuartroper@gmail.com>
2019-04-14 21:08:46 +0100

    menu bar and home page start

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -3,13 +3,19 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "../Home";
 import About from "../About";
 import Contact from "../Contact";
+import css from '../App/app.module.css'
+import twitterBird from '../../images/twitter-circle.png';
+import githubLogo from '../../images/github-logo.png';
+import linkedInLogo from '../../images/LinkedInlogo.png';
+import facebookLogo from '../../images/facebook.png';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <div className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className={css.navBar}>
+            <div className={css.naviComponents}>
             <Link to={"/"} className="nav-link">
               Home
             </Link>
@@ -21,8 +27,14 @@ class App extends Component {
             <Link to={"/about"} className="nav-link">
               About
             </Link>
+            </div>
+            <div className={css.contactIconsMenu}>
+              <img className={css.twitterIcon} src={twitterBird} alt="twitter-bird"/>
+              <img className={css.gitHubIcon} src={githubLogo} alt="github-logo"/>
+              <img className={css.linkedInIcon} src={linkedInLogo} alt="linkedIn-logo"/>
+              <img className={css.facebookIcon} src={facebookLogo} alt="facebook-logo"/>
+            </div>
           </div>
-          <hr />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/contact" component={Contact} />
